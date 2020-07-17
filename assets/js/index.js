68d5e1bd5d5630bfd6108644ab5b1b0fef0bce42
Sunjevaric <usunjevaric@gmail.com>
2020-07-17 23:54:06 +0200

    Added more project to portfolio, bug and typo fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,19 @@
-window.addEventListener('scroll', openScrollMenu)
+window.addEventListener("scroll", openScrollMenu);
 
 //remove class slideFrom top from elements after animaton finished
-LASTMENULINK.addEventListener('animationend', () => {
-  MENULINKS.forEach(item => item.classList.remove('slideFromTop'))
-})
+LASTMENULINK.addEventListener("animationend", () => {
+  MENULINKS.forEach((item) => item.classList.remove("slideFromTop"));
+});
 
 //fade animation on section on scroll
 let isScrolling = false;
-window.addEventListener('scroll', throttleScroll, false);
+window.addEventListener("scroll", throttleScroll, false);
 
 function throttleScroll(e) {
   if (isScrolling === false) {
     window.requestAnimationFrame(() => {
       animateElements(e);
-      isScrolling = false
+      isScrolling = false;
     });
   }
   isScrolling = true;
@@ -24,102 +24,126 @@ function animateElements() {
     ABOUTCONTENT.classList.add("slideBottom");
   }
   if (isElementVisible(EXPIRIENCESECTION)) {
-    EXPIRIENCECONTENT.classList.add('slideBottom');
+    EXPIRIENCECONTENT.classList.add("slideBottom");
   }
   if (isElementVisible(WORKSECTION)) {
-    WORKCONTENT.classList.add('slideBottom');
-    WORKCONTENT.addEventListener('animationend', () => {
-      PORTFOLIOCARD.forEach(card => {
-        card.classList.add('slideInCard')
-      })
+    WORKCONTENT.classList.add("slideBottom");
+    WORKCONTENT.addEventListener("animationend", () => {
+      PORTFOLIOCARD.forEach((card) => {
+        card.classList.add("slideInCard");
+      });
     });
   }
   if (isElementVisible(CONTACTSECTION)) {
-    CONTACTCONTENT.classList.add('slideBottom')
+    CONTACTCONTENT.classList.add("slideBottom");
   }
 }
 
 function isElementVisible(el) {
   const elementBoundary = el.getBoundingClientRect();
   const top = elementBoundary.top;
-  return ((top <= 250));
+  return top <= 250;
 }
 
-HAMBURGER.addEventListener('click', togglePhoneMenu)
+HAMBURGER.addEventListener("click", togglePhoneMenu);
 
 function togglePhoneMenu() {
-  PHONEBACKDROP.classList.toggle('showMenu');
-  PHONENAVIGATION.classList.toggle('showMenu');
-  HAMBURGER.classList.toggle('hamburger__close');
-  BODY.classList.toggle('blur');
+  PHONEBACKDROP.classList.toggle("showMenu");
+  PHONENAVIGATION.classList.toggle("showMenu");
+  HAMBURGER.classList.toggle("hamburger__close");
+  BODY.classList.toggle("blur");
 }
 
-PHONEMENUITEMS.forEach(item => item.addEventListener('click', togglePhoneMenu));
-PHONENAVLOGO.addEventListener('click', togglePhoneMenu);
-
-
+PHONEMENUITEMS.forEach((item) => item.addEventListener("click", togglePhoneMenu));
+PHONENAVLOGO.addEventListener("click", togglePhoneMenu);
 
 function openScrollMenu() {
   if (window.pageYOffset > 90) {
-    NAVIGATION.classList.add('navigation--scroll')
+    NAVIGATION.classList.add("navigation--scroll");
   } else {
-    NAVIGATION.classList.remove('navigation--scroll')
+    NAVIGATION.classList.remove("navigation--scroll");
   }
 }
 
 //fetch number of my public repos
 (() => {
-  const svg = document.querySelector('.repos');
+  const svg = document.querySelector(".repos");
 
-  fetch('https://api.github.com/users/usunjevaric')
-    .then(res => {
-      return res.json()
+  fetch("https://api.github.com/users/usunjevaric")
+    .then((res) => {
+      return res.json();
     })
-    .then(data => {
+    .then((data) => {
       const repos = data.public_repos;
-      const span = document.createElement('span')
-      span.textContent = repos
+      const span = document.createElement("span");
+      span.textContent = repos;
       svg.append(span);
-    })
+    });
 })();
 
 function toggleModal() {
-  MODALBG.classList.toggle('modal-bg--active');
-  BODY.classList.toggle('blur');
+  MODALBG.classList.toggle("modal-bg--active");
+  BODY.classList.toggle("blur");
 }
 function fillModal(card) {
-  const { title, text, codeLink, liveLink, image, tech } = projects[card]
+  const { title, text, codeLink, liveLink, image, tech, contact } = projects[card];
+  console.log(codeLink);
   MODALTITLE.textContent = title;
   MODALTEXT.textContent = text;
-  CODELINK.setAttribute('href', `${codeLink}`);
-  LIVELINK.setAttribute('href', `${liveLink}`);
 
-  MODALIMAGE.setAttribute('src', image);
-  let list = '';
-  tech.forEach(el => {
+  if (codeLink !== "") {
+    CODELINK.style.display = "block";
+    CODELINK.setAttribute("href", `${codeLink}`);
+  } else {
+    CODELINK.style.display = "none";
+  }
+  if (codeLink !== "") {
+    LIVELINK.style.display = "block";
+    LIVELINK.setAttribute("href", `${liveLink}`);
+  } else {
+    LIVELINK.style.display = "none";
+  }
+  if (contact == false) {
+    CODECONTACT.style.display = "none";
+  } else {
+    CODECONTACT.style.display = "block";
+  }
+
+  MODALIMAGE.setAttribute("src", image);
+  let list = "";
+  tech.forEach((el) => {
     list += `<li class='modal__list__item'>${el}</li>`;
-  })
+  });
   TECHLIST.innerHTML = list;
 }
+
+CODECONTACT.addEventListener("click", (e) => {
+  window.location.hash = "#work";
+  e.preventDefault();
+  toggleModal();
+  console.log(window.location);
+  window.location.hash = "#contact";
+});
 //open modal
-PORTFOLIOCARD.forEach(card => {
-  card.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('card__link') && e.target.tagName !== 'path' && e.target.tagName !== 'svg' && !e.target.classList.contains('card__header')) {
+PORTFOLIOCARD.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    if (
+      !e.target.classList.contains("card__link") &&
+      e.target.tagName !== "path" &&
+      e.target.tagName !== "svg" &&
+      !e.target.classList.contains("card__header")
+    ) {
       toggleModal();
-      fillModal(parseInt(e.target.dataset.card) - 1)
+      fillModal(parseInt(e.target.dataset.card) - 1);
     }
+  });
+});
 
-  })
-})
-
-MODALBG.addEventListener('click', (e) => {
+MODALBG.addEventListener("click", (e) => {
   //check to close modal just after click outside of modal content
-  if (e.target.classList.contains('modal-bg')) {
-    toggleModal()
+  if (e.target.classList.contains("modal-bg")) {
+    toggleModal();
   }
-})
+});
 
-MODALCLOSE.addEventListener('click', toggleModal)
-
-
-
+MODALCLOSE.addEventListener("click", toggleModal);
--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,51 +1,92 @@
 const projects = [
   {
-    title: "Interactive Landing Page",
-    text: "Nice looking landing page with live clock.Background image and greeting sentence changes based on the current time. Also user has the option to add his name and current task",
-    codeLink: "https://github.com/usunjevaric/landingClock",
-    liveLink: "https://usunjevaric.github.io/landingClock/",
-    image: './assets/images/Landing.jpg',
-    tech: ['HTML', 'CSS', 'JavaScritp']
-  },
-  {
     title: "TicTacToe",
-    text: "My first project in React, a famous game I used to practice what i learn about React.The style is written using a sass preprocessor. Players can enter names, nice animation for winner and much more",
+    text:
+      "My first project in React, a famous game I used to practice what i learn about React.The style is written using a sass preprocessor. Players can enter names, nice animation for winner and much more. Check out!",
     codeLink: "https://github.com/usunjevaric/tictactoe",
-    liveLink: "",
-    image: './assets/images/tictactoe.jpg',
-    tech: ['HTML', 'SCSS', 'JavaScritp', 'React', 'npm', 'Redux']
+    liveLink: "https://tictactoe-usunjevaric.netlify.app/",
+    contact: false,
+    image: "./assets/images/tictactoe.jpg",
+    tech: ["HTML", "SCSS", "JavaScritp", "React", "npm", "Redux"],
   },
   {
     title: "Social App",
-    text: "In this project i follow FreeCodeCamp.org. Application also use back-end written with Node.js. For storage is used firebase. Registration, login, change profile picture, add post and comments, live notifications and much more",
+    text:
+      "In this project i follow FreeCodeCamp.org. Application also use back-end written with Node.js. For storage is used firebase. Registration, login, change profile picture, add post and comments, live notifications and much more",
     codeLink: "https://github.com/usunjevaric/socialAppFront",
-    liveLink: "",
-    image: './assets/images/social.jpg',
-    tech: ['HTML', 'SCSS', 'JavaScritp', 'React', 'npm', 'Redux', 'Firebase', 'React Router']
+    liveLink: "https://socialapp-usunjevaric.netlify.app/",
+    contact: false,
+    image: "./assets/images/social.jpg",
+    tech: ["HTML", "SCSS", "JavaScritp", "React", "npm", "Redux", "Firebase", "React Router"],
+  },
+  {
+    title: "Weather App",
+    text:
+      "With this app you can look up the weather by city name, application also take your location and load weather for current city.",
+    codeLink: "https://github.com/usunjevaric/weatherApp",
+    liveLink: "https://weatherapp-usunjevaric.netlify.app/",
+    contact: false,
+    image: "./assets/images/weather.jpg",
+    tech: ["CSS", "JavaScritp", "React", "axios", "OpenWeatherMap API"],
+  },
+  {
+    title: "Interactive Landing Page",
+    text:
+      "Nice looking landing page with live clock.Background image and greeting sentence changes based on the current time. Also user has the option to add his name and current task",
+    codeLink: "https://github.com/usunjevaric/landingClock",
+    liveLink: "https://landing-usunjevaric.netlify.app/",
+    contact: false,
+    image: "./assets/images/Landing.jpg",
+    tech: ["HTML", "CSS", "JavaScritp"],
+  },
+  {
+    title: "Simple ToDoList",
+    text:
+      "One of the first project which I make with react.js. Simple application with an option for adding new tasks, mark finished, and delete a task.  Options for hiding finished tasks also included",
+    codeLink: "https://github.com/usunjevaric/react-todolist",
+    liveLink: "https://reacttodolist-usunjevaric.netlify.app/",
+    contact: false,
+    image: "./assets/images/todolist.jpg",
+    tech: ["HTML", "CSS", "React.js"],
   },
   {
     title: "Dice Game",
-    text: "Dice game for two players. All the rules are written in the game. Player roll the dice and collect points. Player collects points by tossing a dice if it gets 1 current result is reset and the next player play. ",
+    text:
+      "Dice game for two players. All the rules are written in the game. Player roll the dice and collect points. Player collects points by tossing a dice if it gets 1 current result is reset and the next player play. ",
     codeLink: "https://github.com/usunjevaric/diceGame",
     liveLink: "https://usunjevaric.github.io/diceGame/dist/",
-    image: './assets/images/dicegame.jpg',
-    tech: ['HTML', 'SCSS', 'JavaScritp']
+    contact: false,
+    image: "./assets/images/dicegame.jpg",
+    tech: ["HTML", "SCSS", "JavaScritp"],
   },
   {
     title: "Old Portfolio",
-    text: "First portfolio site. Created only with HTML and CSS. Some JavaScript used for animation and modal box ",
+    text:
+      "First portfolio site. Created only with HTML and CSS. Some JavaScript used for animation and modal box ",
     codeLink: "https://github.com/usunjevaric/v1",
     liveLink: "https://usunjevaric.github.io/v1/",
-    image: './assets/images/protfoliov1.jpg',
-    tech: ['HTML', 'SCSS', 'JavaScritp']
+    contact: false,
+    image: "./assets/images/protfoliov1.jpg",
+    tech: ["HTML", "SCSS", "JavaScritp"],
   },
   {
-    title: "Garderober",
-    text: "Full stack web application, created as project on faculty. Registration, login, add stuff for selling, pagination, user types and much more. After registration, user can login to account and start with selling. Upload images also works fine",
-    codeLink: "https://github.com/usunjevaric/Garderober",
+    title: "Second-hand shop",
+    text:
+      "Full stack web application, created as project on faculty. Registration, login, add stuff for selling, pagination, user types and much more. After registration, user can login to account and start with selling. User can add item for sale, chose category, location, upload images",
+    codeLink: "",
     liveLink: "",
-    image: './assets/images/garderober.jpg',
-    tech: ['HTML', 'SCSS', 'JavaScritp', 'PHP 7', 'MySQL']
+    contact: true,
+    image: "./assets/images/garderober.jpg",
+    tech: ["HTML", "SCSS", "JavaScritp", "PHP 7", "MySQL"],
   },
-
+  {
+    title: "Report a problem",
+    text:
+      "Another full-stack web application as a faculty project. The main usage of the application is for reporting problems in faculty building. Faculty persons who find some problem can log in with a personal account, and report a problem. After that workers can see a report and start with resolving. Admins have statistic and some extra benefits.",
+    codeLink: "",
+    liveLink: "",
+    contact: true,
+    image: "./assets/images/prijavakvarova.jpg",
+    tech: ["HTML", "SCSS", "Angular 8", ".NET Core", "MySQL", "Bootstrap"],
+  },
 ];
